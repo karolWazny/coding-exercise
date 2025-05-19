9eedf8421a305873660a36c33294c6ac85e65533
Karol Ważny <karoljanwazny@gmail.com>
2025-05-19 21:35:29 +0200

    Added first test stub

--- src/test/java/online/niepowazni/scoreboard/ScoreBoardImplTest.java
+++ src/test/java/online/niepowazni/scoreboard/ScoreBoardImplTest.java
@@ -1,4 +1,14 @@
 package online.niepowazni.scoreboard;
 
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
 public class ScoreBoardImplTest {
+
+    @Test
+    @DisplayName("Initial test")
+    public void initialTest() {
+        Assertions.assertTrue(true);
+    }
 }
