5bb5265e03296ac8319f5ef2961ba6c987fad3d3
Karol Ważny <karoljanwazny@gmail.com>
2025-05-19 22:18:58 +0200

    refactor game tests

--- src/test/java/online/niepowazni/scoreboard/GameTest.java
+++ src/test/java/online/niepowazni/scoreboard/GameTest.java
@@ -1,7 +1,6 @@
 package online.niepowazni.scoreboard;
 
 import org.junit.jupiter.api.Assertions;
-import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.params.ParameterizedTest;
@@ -9,19 +8,13 @@ import org.junit.jupiter.params.provider.CsvSource;
 
 public class GameTest {
 
-    private Game game;
-
-    @BeforeEach
-    public void prepareGame() {
-        game = Game.builder()
-                .homeTeam("Sweden")
-                .awayTeam("Denmark")
-                .build();
-    }
-
     @Test
     @DisplayName("Fresh game should have 0 - 0 score")
     public void freshGameShouldBeZeroZero() {
+        Game game = Game.builder()
+                .homeTeam("Sweden")
+                .awayTeam("Denmark")
+                .build();
         Assertions.assertEquals(0, game.getHomeTeamScore());
         Assertions.assertEquals(0, game.getAwayTeamScore());
     }
@@ -33,7 +26,7 @@ public class GameTest {
     })
     @DisplayName("Game should be displayed in specific format")
     public void gameShouldBeFormattedForDisplay(String homeTeam, String awayTeam) {
-        game = Game.builder()
+        Game game = Game.builder()
                 .homeTeam(homeTeam)
                 .awayTeam(awayTeam)
                 .build();
